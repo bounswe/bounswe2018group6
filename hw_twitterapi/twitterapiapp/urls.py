e8aee26379be49ce343d8de2e8bff0f8ce66f792
hmente <36155559+hmente@users.noreply.github.com>
2018-04-13 22:50:56 +0300

    Update urls.py

--- hw_twitterapi/twitterapiapp/urls.py
+++ hw_twitterapi/twitterapiapp/urls.py
@@ -3,15 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # from the root: /
-    # main page
-    path('', views.main_page),
+    # from the root: api/friends/user_id
+    path ( 'api/find_friends/<str:user_id>', views.find_friends_api ),
 
-    # from the root: /api/timegraph/user_id
-    path('api/timegraph/<str:user_id>', views.timegraph_api),
+    # from the root: /find_friends/user_id
+    path ( 'find_friends/<str:user_id>', views.find_friends ),
 
-    # from the root: /timegraph/user_id
-    path('timegraph/<str:user_id>', views.timegraph),
-
-    # TODO Add two paths for each Twitter endpoint, one for the API and the other for the frontend.
-]
