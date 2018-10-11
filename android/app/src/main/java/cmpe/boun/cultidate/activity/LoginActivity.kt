d6bbc642c6e684a7f829ee890dcddd425ed8b988
Atıf Emre Yüksel <atifemreyuksel@gmail.com>
2018-10-11 23:57:34 +0300

    Add simple login page for a start

--- android/app/src/main/java/cmpe/boun/cultidate/activity/LoginActivity.kt
+++ android/app/src/main/java/cmpe/boun/cultidate/activity/LoginActivity.kt
@@ -1,13 +1,31 @@
 package cmpe.boun.cultidate.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 import cmpe.boun.culdidate.R
+import cmpe.boun.cultidate.activity.ForgotPassActivity
+import cmpe.boun.cultidate.activity.RegisterActivity
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val registerButton = findViewById<Button>(R.id.register_button)
+
+        registerButton.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+        val forgetButton = findViewById<Button>(R.id.forget_button)
+
+        forgetButton.setOnClickListener {
+            val intent = Intent(this, ForgotPassActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
