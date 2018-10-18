d6eb86e657bb039b42c409b1221d701d4ade8a74
Unknown <atifemreyuksel@gmail.com>
2018-10-18 21:12:28 +0300

    Add the control of whether password is valid or not

--- android/app/src/main/java/cmpe/boun/cultidate/activity/LoginActivity.kt
+++ android/app/src/main/java/cmpe/boun/cultidate/activity/LoginActivity.kt
@@ -18,6 +18,7 @@ class LoginActivity : AppCompatActivity() {
         val registerButton = findViewById<Button>(R.id.register_button)
         val forgetButton = findViewById<Button>(R.id.forget_button)
         val mail = findViewById<EditText>(R.id.text_mail)
+        val password = findViewById<EditText>(R.id.text_password)
         val loginButton = findViewById<Button>(R.id.login_button)
 
         forgetButton.setOnClickListener {
@@ -34,6 +35,10 @@ class LoginActivity : AppCompatActivity() {
             if (!isEmailValid(mail.text)) {
                 Toast.makeText(applicationContext, "Invalid email address!", Toast.LENGTH_SHORT).show()
             }
+            else if (!isPassValid(password.text)) {
+                Toast.makeText(applicationContext, "Password has at least 6 characters, at least one digit," +
+                        " and at least one uppercase letter! ", Toast.LENGTH_SHORT).show()
+            }
             else {
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
@@ -44,5 +49,10 @@ class LoginActivity : AppCompatActivity() {
 
     fun isEmailValid(email: CharSequence): Boolean {
         return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    fun isPassValid(password: CharSequence): Boolean {
+        val regex = Regex(pattern = "(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])")
+        return password.length > 6 && password.contains(regex)
     }
 }
