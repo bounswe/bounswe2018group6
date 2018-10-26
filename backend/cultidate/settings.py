f44510b7375b2ea65e51229e9552da8f0050f89d
cerob <cerob@users.noreply.github.com>
2018-10-26 16:50:16 +0300

    Add email server settings to settings.py

--- backend/cultidate/settings.py
+++ backend/cultidate/settings.py
@@ -117,6 +117,19 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+# Email Server
+
+EMAIL_USE_TLS = True
+
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+EMAIL_PORT = os.getenv('EMAIL_PORT', 587)
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
