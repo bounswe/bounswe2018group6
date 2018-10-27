ce8ebe05939029127af1584c48a0ff6c0b42b563
Omer Kirbiyik <kirbiyikomer@gmail.com>
2018-10-27 19:12:24 +0300

    Fix import problems

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,10 +1,8 @@
 import LandingPage from "views/Home/LandingPage.jsx";
-import ProfilePage from "views/ProfilePage/ProfilePage.jsx";
 import LoginPage from "views/LoginPage/LoginPage.jsx";
 import SignUpPage from "views/LoginPage/SignUpPage.jsx";
 
 var indexRoutes = [
-  { path: "/profile-page", name: "ProfilePage", component: ProfilePage },
   { path: "/login", name: "LoginPage", component: LoginPage },
   { path: "/sign-up", name: "SignUpPage", component: SignUpPage },
   { path: "/", name: "LandingPage", component: LandingPage }
