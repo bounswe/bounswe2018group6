d90dd4330aedef0664dccd7ffef8ae693d87282f
Atıf Emre Yüksel <atifemreyuksel@gmail.com>
2018-10-29 18:20:12 +0300

    Add retrofit service object and create api interface

--- android/app/src/main/java/cmpe/boun/cultidate/activity/LoginActivity.kt
+++ android/app/src/main/java/cmpe/boun/cultidate/activity/LoginActivity.kt
@@ -6,8 +6,24 @@ import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.EditText
 import cmpe.boun.culdidate.R
-import cmpe.boun.cultidate.MainActivity
 import android.widget.Toast
+import retrofit2.Call
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.Retrofit
+import retrofit2.Callback
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
+
+class AuthResponse {
+    @SerializedName("token")
+    @Expose
+    private var token: String = ""
+
+    fun getToken(): String {
+        return token
+    }
+}
 
 class LoginActivity : AppCompatActivity() {
 
@@ -17,9 +33,11 @@ class LoginActivity : AppCompatActivity() {
 
         val registerButton = findViewById<Button>(R.id.register_button)
         val forgetButton = findViewById<Button>(R.id.forget_button)
-        val mail = findViewById<EditText>(R.id.text_mail)
         val password = findViewById<EditText>(R.id.text_password)
+        val username = findViewById<EditText>(R.id.text_username)
         val loginButton = findViewById<Button>(R.id.login_button)
+
+        val service = createService()
 
         forgetButton.setOnClickListener {
             val intent = Intent(this, ForgotPassActivity::class.java)
@@ -32,23 +50,39 @@ class LoginActivity : AppCompatActivity() {
         }
 
         loginButton.setOnClickListener {
-            if (!isEmailValid(mail.text)) {
-                Toast.makeText(applicationContext, "Invalid email address!", Toast.LENGTH_SHORT).show()
-            }
-            else if (!isPassValid(password.text)) {
+            /*if (!isPassValid(password.text)) {
                 Toast.makeText(applicationContext, "Password has at least 6 characters, at least one digit," +
-                        " and at least one uppercase letter! ", Toast.LENGTH_SHORT).show()
+                       " and at least one uppercase letter! ", Toast.LENGTH_SHORT).show()
+            }*/
+
+            val user = User(username.text.toString(), password.text.toString())
+            val userResponse = service.authenticate(user).execute()
+
+            if (userResponse.isSuccessful) {
+                val token = userResponse.body()!!.getToken()
+                Toast.makeText(applicationContext, "Call is successful", Toast.LENGTH_SHORT).show()
             }
             else {
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
+                Toast.makeText(applicationContext, "Call is not successful", Toast.LENGTH_SHORT).show()
             }
-        }
 
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 
-    fun isEmailValid(email: CharSequence): Boolean {
-        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    fun createService(): ApiInterface {
+        val BASE_URL = "http://cultidate.herokuapp.com/"
+        val retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+        val service = retrofit.create(ApiInterface::class.java)
+
+        return service
     }
 
     fun isPassValid(password: CharSequence): Boolean {
