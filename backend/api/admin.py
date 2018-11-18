37feeef3ed0f57502dc0439d502ad0067ed6239e
cerob <cerob@users.noreply.github.com>
2018-11-18 13:16:53 +0300

    add User to Admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import (User as CustomUser, CorporateUserProfile, Event, AttendanceStatus,
+                     Comment, FollowStatus, Location, Media, Tag, VoteStatus)
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    '''
+    # manual model display
+    fields = ('id', 
+              ('is_active', 'is_superuser'),
+              ('is_staff', 'is_corporate_user'), 
+              ('last_login', 'date_joined'),
+              'username', 'email', ('first_name', 'last_name'), 
+              'bio', 'city', 'corporate_profile', 
+              'follower_count', 'vote_count')
+    fieldsets = None
+    '''
+    # imported from django.contrib.admin.models
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('email', 'first_name', 'last_name',
+                                      'bio', 'city', 'follower_count', 'vote_count')}),
+        ('Corporate User', {'fields': ('is_corporate_user', 'corporate_profile')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    readonly_fields = ('id', 'last_login', 'date_joined', 'follower_count', 'vote_count')
+
+    ordering = ('-id',)
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name',
+                    'is_corporate_user', 'follower_count', 'vote_count')
+    list_editable = ('first_name', 'last_name')
+    list_filter = ('is_active', 'is_superuser', 'is_staff', 'is_corporate_user', 'city')
