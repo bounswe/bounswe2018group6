bd54eed5b7a2c0fa168107a231c67f3d8ae318a8
Ömer Kırbıyık <kirbiyikomer@gmail.com>
2018-11-23 23:50:22 +0300

    Recommit: Add media functionality to events

--- frontend/src/views/dashboard/admin/components/BoxCard.vue
+++ frontend/src/views/dashboard/admin/components/BoxCard.vue
@@ -2,7 +2,7 @@
   <el-card class="box-card-component" style="margin-left:8px;">
     <div slot="header" class="box-card-header">
     <router-link  :to="'/' + eventLink">
-      <img src="https://wpimg.wallstcn.com/e7d23d71-cf19-4b90-a1cc-f56af8c0903d.png">
+      <img :src="image">
     </router-link>
     </div>
     <div style="position:relative;">
@@ -63,6 +63,9 @@ export default {
     },
     votes: {
       type: Number,
+    },
+    image: {
+      type: String,
     },
   },
   filters: {
