cf0924bad232b841f592e954b1dfe9b136d53946
Atıf Emre Yüksel <atifemreyuksel@gmail.com>
2018-11-26 21:55:11 +0300

    Insert the design and functionality of attendance button

--- frontend/src/api/event.js
+++ frontend/src/api/event.js
@@ -10,12 +10,42 @@ export function fetchEvents() {
 }
 
 export function getEventDetail(event_id) {
-    return request({
-      url: '/events/' + event_id + '/',
-      method: 'get'
-    })
+  const headers = {
+    //ContentType: 'application/json',
+    Authorization: 'Token ' + getToken(),
+  }
+  return request({
+    headers,
+    url: '/events/' + event_id + '/',
+    method: 'get'
+  })
   }
 
+export function attendance(event, status) {
+  const headers = {
+    //ContentType: 'application/json',
+    Authorization: 'Token ' + getToken(),
+  }
+  let attend;
+  attend = {
+    "Attend": "Y",
+    "Won't attend": "N",
+    "Maybe": "M",
+    "Block": "B",
+  }
+  status = attend[status]
+
+  const data = {
+    event,
+    status
+  }
+  return request({
+    headers,
+    url: '/attendance/',
+    method: 'post',
+    data
+  })
+}
 
 export function createEvent(data) {
     const headers = {
--- frontend/src/views/events/showEvent.vue
+++ frontend/src/views/events/showEvent.vue
@@ -1,38 +1,36 @@
 <template>
   <div class="components-container">
     <el-row>
-      <!-- <img height="340" width="480"  position=relative; src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQcQqPq3suSxF5X_e9OpVQxp-Y8x3eiHmldlcKFHw2SxRfSRmj"> -->
-      <el-carousel trigger="click" height="350px">
-        <el-carousel-item v-for="media in eventDetails.medias" v-bind:key="media">
-          <img style="object-fit: contain;" :src="media.url">
-        </el-carousel-item>
-      </el-carousel>
       <el-col :span="14"><div class="grid-content bg-purple-light">
         <center style="font-size: 25px; font-weight:bold"> {{ eventDetails.title }} </center>
         <p style="font-size: 20px; margin-left: 20px;">by {{ eventDetails.owner.username}} </p>
         <p style="font-size: 20px; margin-left: 20px; font-weight:bold;">Date and Time</p>
         <p style="font-size: 20px; margin-left: 20px;">{{ eventDetails.date }}</p>
         <div style="font-size: 20px; margin-left: 20px; margin-top: 130px;"> <span>Price: {{ eventDetails.price }} </span>
-          <el-radio-group v-model="radio4" size="small" style="margin-left: 190px; margin-right: 20px;">
-            <el-radio-button label="Attend" />
-            <el-radio-button label="Maybe"/>
-            <el-radio-button label="Won't attend"/>
-            <el-radio-button label="Block"/>
-          </el-radio-group>
+          <el-select v-model="attend" placeholder="Attendance" style="margin-right: 20px; float: right;" v-on:change="attendanceEvent">
+            <el-option v-for="item in options" :key="item.attend" :label="item.label" :value="item.attend"></el-option>
+          </el-select>
         </div>
       </div>
       </el-col>
+      <el-carousel trigger="click" height="350px">
+        <el-carousel-item v-for="media in eventDetails.medias" v-bind:key="media">
+          <img style="object-fit: contain;" :src="media.url">
+        </el-carousel-item>
+      </el-carousel>  
     </el-row>
     <div style="margin-top: 30 px" >
       <p/>
       <el-tag v-for="tag in tags" :key="tag" size="medium">
         {{ tag }}
       </el-tag>
-      <el-button style="float: right;" type="primary" plain size="medium">Follow</el-button>
+      <el-button style="float: right;" type="primary" plain size="medium" @click.native.prevent="followEvent">{{following}}</el-button>
     </div>
     <div style="margin-top: 20px">
-      <span style="font-size: 20px; font-weight:bold;">{{ eventDetails.description }}</span>
+      <span style="font-size: 20px; font-weight:bold;">Description</span>
       <el-rate v-model="rate" :colors="['#99A9BF', '#F7BA2A', '#FF9900']" style="float: right;"/>
+      <p>{{ eventDetails.description }} </p>
+      
       
     </div>
 
@@ -40,28 +38,65 @@
 </template>
 
 <script>
-import { getEventDetail } from '@/api/event'
-
+import { getEventDetail, follow, unfollow, attendance } from '@/api/event'
 export default {
   name: 'ShowEvent',
   components: {},
   data() {
     return {
-      radio4: 'Attend',
+      options: [{
+          attend: 'Attend',
+          label: 'Attend'
+        }, {
+          attend: 'Won\'t attend',
+          label: 'Won\'t attend'
+        }, {
+          attend: 'Maybe',
+          label: 'Maybe'
+        }, {
+          attend: 'Block',
+          label: 'Block'
+        }
+      ],
+      attend: '',
       rate: null,
       tags: ['food', 'culture', 'kebap'],
       eventDetails: null,
+      following: null,
+      event_id: null,
+      follow_event_id: null
     }
   },
   created() {
-    const event_id = this.$route.params.id
-    this.fetchData(event_id)
+    this.event_id = this.$route.params.id
+    this.fetchData(this.event_id)
   },
   methods: {
     fetchData(event_id) {
+      let attends;
+      attends = {
+        "Y": "Attend",
+        "N": "Won't attend",
+        "M": "Maybe",
+        "B": "Block"
+      }
       getEventDetail(event_id).then(response => {
         this.eventDetails = response.data
-        console.log(this.eventDetails)
+        if (response.data.own_attendance_status != null) {
+          this.attend = attends[response.data.own_attendance_status.status]
+        }
+      })
+    },
+    attendanceEvent() {
+      let attends;
+      attends = {
+        "Y": "Attend",
+        "N": "Won't attend",
+        "M": "Maybe",
+        "B": "Block"
+      }
+      attendance(this.event_id, this.attend).then(response => {
+          this.attend = attends[response.data.status]   
       })
     }
   }
@@ -73,10 +108,8 @@ export default {
     position: relative;
     height: 100vh;
   }
-
   .el-col {
     border-radius: 4px;
-
   }
   .bg-purple-dark {
     background: #1872f0;
@@ -89,7 +122,7 @@ export default {
   }
   .grid-content {
     border-radius: 4px;
-    min-height: 340px;
+    min-height: 350px;
   }
   .row-bg {
     padding: 10px 0;
