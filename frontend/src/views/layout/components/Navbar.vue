f89fdae15a92f843165039b3f0d7c33fbd0490f3
Ömer Kırbıyık <kirbiyikomer@gmail.com>
2018-11-28 01:41:37 +0300

    Fix GitHub link

--- frontend/src/views/layout/components/Navbar.vue
+++ frontend/src/views/layout/components/Navbar.vue
@@ -34,7 +34,7 @@
               {{ $t('navbar.dashboard') }}
             </el-dropdown-item>
           </router-link>
-          <a target="_blank" href="https://github.com/PanJiaChen/vue-element-admin/">
+          <a target="_blank" href="https://github.com/bounswe/bounswe2018group6">
             <el-dropdown-item>
               {{ $t('navbar.github') }}
             </el-dropdown-item>
