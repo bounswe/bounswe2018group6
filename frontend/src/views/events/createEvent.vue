2ba3e978388268122c92f69fa4c6171ab96bdafc
Ömer Kırbıyık <kirbiyikomer@gmail.com>
2018-12-03 19:28:08 +0300

    Fix createEvent form fields

--- frontend/src/views/events/createEvent.vue
+++ frontend/src/views/events/createEvent.vue
@@ -1,38 +1,44 @@
 <template>
-<el-form ref="form" :model="form" label-width="200px" label-position="top" style="margin:20px">
-  <el-form-item label="Title of event">
-    <el-input v-model="form.title"></el-input>
-  </el-form-item>
-  <el-form-item label="Description of event">
-    <el-input v-model="form.description"></el-input>
-  </el-form-item>
-  <el-form-item label="Date and time">
-    <el-date-picker
-        v-model="form.date"
-        type="datetime"
-        placeholder="Select date and time">
-    </el-date-picker>
-  </el-form-item>
-  <el-form-item label="Price, enter 0 if free.">
-    <el-input type="number" v-model="form.price"></el-input>
-  </el-form-item>
-  <el-form-item label="Website">
-    <el-input v-model="form.organizer_url">
-    </el-input>
-  </el-form-item>
-  <el-form-item label="Artists - NOT AVAILABLE">
-    <el-input v-model="form.artists"></el-input>
-  </el-form-item>
-  <el-form-item label="Medias- NOT AVAILABLE">
-    <el-input v-model="form.medias"></el-input>
-  </el-form-item>
-  <el-form-item label="Tags">
-    <el-input v-model="form.tags"></el-input>
-  </el-form-item>
-  <el-form-item>
-    <el-button type="primary" @click="onSubmit">Create</el-button>
-  </el-form-item>
-</el-form>
+<div class="tab-container">
+  <el-form ref="form" :model="form" label-width="200px" label-position="top" style="margin:20px">
+    <el-form-item label="Title of event">
+      <el-input v-model="form.title"></el-input>
+    </el-form-item>
+    <el-form-item label="Description of event">
+      <el-input v-model="form.description"></el-input>
+    </el-form-item>
+    <el-form-item label="Date and time">
+      <el-date-picker
+          v-model="form.date"
+          type="datetime"
+          placeholder="Select date and time">
+      </el-date-picker>
+    </el-form-item>
+    <el-form-item label="Price, enter 0 if free.">
+      <el-input type="number" v-model="form.price">
+        <template slot="append">₺</template>
+      </el-input>
+    </el-form-item>
+    <el-form-item label="Website">
+      <el-input v-model="form.organizer_url">
+      </el-input>
+    </el-form-item>
+    <el-form-item label="City">
+      <el-input v-model="form.location.city">
+      </el-input>
+    </el-form-item>
+    <el-form-item label="District">
+      <el-input v-model="form.location.district">
+      </el-input>
+    </el-form-item>
+    <el-form-item label="Tags">
+      <el-input v-model="form.tags"></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">Create</el-button>
+    </el-form-item>
+  </el-form>
+</div>
 </template>
 
 
@@ -46,10 +52,12 @@ import { createEvent } from '@/api/event'
           title: '',
           description: '',
           date: '',
-          price: '',
+          price: null,
           organizer_url: '',
-          artist: '',
-          medias: [],
+          location: {
+            city: '',
+            district: '',
+          },
           tags: []
         }
       }
