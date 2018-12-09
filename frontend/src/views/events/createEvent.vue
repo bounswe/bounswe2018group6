a8d33401f3a3e049385871db83b232d333a6cddc
atifemreyuksel <atifemreyuksel@gmail.com>
2018-12-09 17:13:30 +0300

    Add functional maps feature for all events page

--- frontend/src/views/events/createEvent.vue
+++ frontend/src/views/events/createEvent.vue
@@ -23,23 +23,44 @@
       <el-input v-model="form.organizer_url">
       </el-input>
     </el-form-item>
-    <el-form-item label="City">
-      <el-input v-model="form.location.city">
-      </el-input>
-    </el-form-item>
-    <el-form-item label="District">
-      <el-input v-model="form.location.district">
-      </el-input>
-    </el-form-item>
     <el-form-item label="Tags">
       <el-checkbox-group v-model="form.tags">
         <el-checkbox-button v-for="tag in tag_list" :label="tag.id" :key="tag.name">{{tag.name}}</el-checkbox-button>
       </el-checkbox-group>
     </el-form-item>
+
+
+    <el-form-item label="Select location">
+        <div>
+          <label>
+            <GmapAutocomplete placeholder="Please insert location" @place_changed="setPlace">
+            </GmapAutocomplete>
+            <el-button style="float: right;" type="primary" @click="usePlace">Add Location</el-button>
+          </label>
+          <br/>
+
+          <GmapMap style="width: 600px; height: 300px; margin-top: 15px;" :zoom="5" :center="{ lat: 41.015137,lng: 28.979530 }">
+            <GmapMarker v-for="(marker, index) in markers"
+              :key="index"
+              :position="marker.position"
+              />
+            <GmapMarker
+              v-if="this.place"
+              label="★"
+              :position="{
+                lat: this.place.geometry.location.lat(),
+                lng: this.place.geometry.location.lng(),
+              }"
+              />
+          </GmapMap>
+        </div>
+    </el-form-item>
+
     <el-form-item>
       <el-button type="primary" @click="onSubmit">Create</el-button>
     </el-form-item>
   </el-form>
+  
 </div>
 </template>
 
@@ -59,16 +80,43 @@ import { createEvent, getTags } from '@/api/event'
           location: {
             city: '',
             district: '',
+            google_place_id: '',
+            name: '',
+            lat: null,
+            lng: null
           },
           tags: []
         },
-        tag_list: []
+        tag_list: [],
+        markers: [],
+        place: null
+
       }
     },
     created() {
       this.getTagList()
     },
     methods: {
+      setPlace(place) {
+        this.place = place
+        this.form.location.city = this.place.address_components[3].long_name
+        this.form.location.district = this.place.address_components[2].long_name
+        this.form.location.name = this.place.name
+        this.form.location.lat = this.place.geometry.location.lat().toFixed(6)
+        this.form.location.lng = this.place.geometry.location.lng().toFixed(6)
+        this.form.location.google_place_id = this.place.id
+      },
+      usePlace(place) {
+        if (this.place) {
+          this.markers.push({
+            position: {
+              lat: this.place.geometry.location.lat(),
+              lng: this.place.geometry.location.lng(),
+            }
+          })
+          this.place = null;
+        }
+      },
       onSubmit() {
         // URL needs to start with http or https as dictated by backend
         if(!this.form.organizer_url.startsWith('http://') &&
