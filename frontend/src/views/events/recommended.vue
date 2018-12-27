5cceb2ab3262932569c7e0e4af8b877d1c3db14b
Atıf Emre Yüksel <atifemreyuksel@gmail.com>
2018-12-28 00:49:25 +0300

    Add location-based and content-based search into recommended page

--- frontend/src/views/events/recommended.vue
+++ frontend/src/views/events/recommended.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="dashboard-editor-container">
+    <el-row style="margin-bottom: 15px;">
+      <el-input placeholder="Please search event" v-model="search_word" class="input-with-select">
+        <el-select v-model="search_type" slot="prepend" placeholder="Search with">
+          <el-option label="Content" value="1"></el-option>
+          <el-option label="Location" value="2"></el-option>
+        </el-select>
+        <el-button slot="append" icon="el-icon-search" @click.native.prevent="searchEvents"></el-button>
+      </el-input>
+    </el-row>
     <el-row :gutter="8">
       <el-col v-for="event in eventList" :key="event.id" :xs="{span: 24}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 6}" style="margin-bottom:30px;">
         <box-card
@@ -20,7 +29,7 @@
 
 <script>
 import BoxCard from '@/views/dashboard/admin/components/BoxCard'
-import { fetchEvents } from '@/api/event'
+import { fetchEvents, getTags, searchByTitle, searchByLocation } from '@/api/event'
 
 export default {
   name: 'DashboardAdmin',
@@ -29,36 +38,60 @@ export default {
   },
   data() {
     return {
-      eventList: null
+      eventList: null,
+      search_type: null,
+      search_word: null
     }
   },
   created() {
     this.fetchData()
+    this.getTagList()
   },
   methods: {
     fetchData() {
       fetchEvents().then(response => {
         this.eventList = response.data
-        console.log(this.eventList)
       })
     },
     beautifyDate(date) {
       var d = new Date(date)
       date = (d.getDate()<10?'0':'') + d.getDate() + "/" + d.getMonth() + "/" + d.getFullYear() + " - " + (d.getHours()<10?'0':'')+  d.getHours() + ":" + (d.getMinutes()<10?'0':'') + d.getMinutes()
       return date
+    },
+    searchEvents() {
+      console.log("search type is " + this.search_type)
+      if(this.search_type == 1) {
+        searchByTitle(this.search_word).then(response => {
+          this.eventList = response.data
+        })
+      } 
+      else if(this.search_type == 2) {
+        searchByLocation(this.search_word).then(response => {
+          this.eventList = response.data
+        })
+      }
     }
   }
 }
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-.dashboard-editor-container {
-  padding: 32px;
-  background-color: rgb(240, 242, 245);
-  .chart-wrapper {
-    background: #fff;
-    padding: 16px 16px 0;
-    margin-bottom: 32px;
+  .dashboard-editor-container {
+    padding: 32px;
+    background-color: rgb(240, 242, 245);
+    .chart-wrapper {
+      background: #fff;
+      padding: 16px 16px 0;
+      margin-bottom: 32px;
+    }
   }
-}
+  .input-with-select .el-input-group__prepend {
+    background-color: #fff;
+  }
+  .el-input {
+    width: 500px;
+  }
+  .el-select {
+    width: 120px;
+  }
 </style>
