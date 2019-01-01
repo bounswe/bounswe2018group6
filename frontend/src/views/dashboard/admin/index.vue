1c8736103cc6c92359ce754ea5b42cd6442f1471
Atıf Emre Yüksel <atifemreyuksel@gmail.com>
2019-01-01 10:40:07 +0300

    Make dashboard functional by adding real values and following list

--- frontend/src/views/dashboard/admin/index.vue
+++ frontend/src/views/dashboard/admin/index.vue
@@ -1,94 +1,65 @@
 <template>
   <div class="dashboard-editor-container">
-
     <github-corner style="position: absolute; top: 0px; border: 0; right: 0;"/>
-
     <panel-group @handleSetLineChartData="handleSetLineChartData"/>
-
-    <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
-      <line-chart :chart-data="lineChartData"/>
-    </el-row>
     <el-row :gutter="32">
-      <!-- <el-col :xs="24" :sm="24" :lg="8">
-        <div class="chart-wrapper">
-          <raddar-chart/>
-        </div>
-      </el-col>
-      <el-col :xs="24" :sm="24" :lg="8">
-        <div class="chart-wrapper">
-          <pie-chart/>
-        </div>
-      </el-col> -->
-      <el-col :xs="24" :sm="24" :lg="8">
-        <div class="chart-wrapper">
-          <bar-chart/>
-        </div>  
-      </el-col>
       <el-col :xs="{span: 24}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 6}" style="margin-bottom:30px;">
         <todo-list/>
       </el-col>
+      <el-col :xs="{span: 24}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 6}" style="margin-bottom:30px;">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span style="font-weight:bold;">Following events</span>
+          </div>
+          <div v-for="event in following_events" :key="event.title" class="text item">
+            <router-link :to="'events/show-event/' + event.event.id"><mallki :text="event.event.title" class-name="mallki-text"/></router-link>
+            <span style="float: right; color: #77450d; font-weight:bold;">{{ beautifyDate(event.event.date) }}</span>
+          </div>
+        </el-card>
+      </el-col>
     </el-row>
-
-      <!-- <el-col :xs="{span: 24}" :sm="{span: 24}" :md="{span: 24}" :lg="{span: 12}" :xl="{span: 12}" style="padding-right:8px;margin-bottom:30px;">
-        <transaction-table/>
-      </el-col> -->
-    
-
   </div>
 </template>
 
 <script>
 import GithubCorner from '@/components/GithubCorner'
 import PanelGroup from './components/PanelGroup'
-import LineChart from './components/LineChart'
-import RaddarChart from './components/RaddarChart'
-import PieChart from './components/PieChart'
-import BarChart from './components/BarChart'
-import TransactionTable from './components/TransactionTable'
 import TodoList from './components/TodoList'
 import BoxCard from './components/BoxCard'
+import Mallki from '@/components/TextHoverEffect/Mallki'
 
-const lineChartData = {
-  newVisitis: {
-    expectedData: [100, 120, 161, 134, 105, 160, 165],
-    actualData: [120, 82, 91, 154, 162, 140, 145]
-  },
-  messages: {
-    expectedData: [200, 192, 120, 144, 160, 130, 140],
-    actualData: [180, 160, 151, 106, 145, 150, 130]
-  },
-  purchases: {
-    expectedData: [80, 100, 121, 104, 105, 90, 100],
-    actualData: [120, 90, 100, 138, 142, 130, 130]
-  },
-  shoppings: {
-    expectedData: [130, 140, 141, 142, 145, 150, 160],
-    actualData: [120, 82, 91, 154, 162, 140, 130]
-  }
-}
+import { getUserInfo } from '@/api/user'
 
 export default {
   name: 'DashboardAdmin',
   components: {
     GithubCorner,
     PanelGroup,
-    LineChart,
-    RaddarChart,
-    PieChart,
-    BarChart,
-    TransactionTable,
     TodoList,
     BoxCard,
+    Mallki
   },
   data() {
     return {
-      lineChartData: lineChartData.newVisitis
+      following_events: null
     }
   },
+  created() {
+    this.userDetails(this.$store.state.user.user_id)
+  },
   methods: {
-    handleSetLineChartData(type) {
-      this.lineChartData = lineChartData[type]
-    }
+    userDetails(user_id) {
+      getUserInfo(user_id).then(response => {
+        this.following_events = response.data.followings.events
+        console.log(this.following_events)
+      })
+    },
+    beautifyDate(date) {
+      var d = new Date(date)
+      date = (d.getDate()<10?'0':'') + d.getDate() + "/" + ((d.getMonth() + 1)<10?'0':'') + (d.getMonth() + 1) + "/" +
+               d.getFullYear() + " - " + (d.getHours()<10?'0':'')+  d.getHours() + ":" + (d.getMinutes()<10?'0':'') + d.getMinutes()
+      return date
+    },
   }
 }
 </script>
@@ -102,5 +73,28 @@ export default {
     padding: 16px 16px 0;
     margin-bottom: 32px;
   }
+}
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both
+}
+.box-card {
+  width: 480px;
+}
+.mallki-text {
+  font-size: 15px;
+  color: black;
 }
 </style>
