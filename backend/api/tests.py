ea43cfc1e85ce850aa5d7d42824bc1336af06280
cerob <cerob@users.noreply.github.com>
2019-01-07 12:36:46 +0300

    Add User related unittests

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,8 +1,65 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
 
-from api import views
 from api.models import (User)
+
+
+class SignupTests(APITestCase):
+    def test_signup(self):
+        """ Tests if a user can signup. """
+        data = {
+            'email': 'testusersignup@cultidate.com',
+            'username': 'testusersignup',
+            'password': '12345678+',
+            'first_name': 'name',
+            'last_name': 'surname',
+            'bio': 'MyBio',
+            'city': 'Istanbul',
+            'is_corporate_user': False,
+            'corporate_profile': None,
+        }
+        response = self.client.post('/api/signup/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertIsInstance(response.data.pop('id'), int)
+        self.assertEqual(response.data.get('email'), data['email'])
+        self.assertEqual(response.data.get('username'), data['username'])
+        self.assertEqual(response.data.get('first_name'), data['first_name'])
+        self.assertEqual(response.data.get('last_name'), data['last_name'])
+        self.assertEqual(response.data.get('bio'), data['bio'])
+        self.assertEqual(response.data.get('city'), data['city'])
+        self.assertEqual(response.data.get('is_corporate_user'), data['is_corporate_user'])
+    
+    def test_prohibit_with_short_password(self):
+        """ Tests if a user can signup with a short password. """
+        data = {
+            'email': 'testusersignup1@cultidate.com',
+            'username': 'testusersignup',
+            'password': '1',
+            'first_name': 'name',
+            'last_name': 'surname',
+            'bio': 'MyBio',
+            'city': 'Istanbul',
+            'is_corporate_user': False,
+            'corporate_profile': None,
+        }
+        response = self.client.post('/api/signup/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    
+    def test_prohibit_with_long_password(self):
+        """ Tests if a user can signup with a short password. """
+        data = {
+            'email': 'testusersignup2@cultidate.com',
+            'username': 'testusersignup',
+            'password': 'asdfghjkloinhytvbhjgtyuij',
+            'first_name': 'name',
+            'last_name': 'surname',
+            'bio': 'MyBio',
+            'city': 'Istanbul',
+            'is_corporate_user': False,
+            'corporate_profile': None,
+        }
+        response = self.client.post('/api/signup/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
 class LoginTests(APITestCase):
